148ae138f91c56005fe469e0b85e9c8b81eccca0
rohit-simform <rohit.s@simformsolutions.com>
2023-10-07 20:30:46 +0530

    add IVS feature

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const uploadRouter = require('./upload');
+const MediaStreamingRouter = require('./media-streaming');
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.json());
 const port = 5000;
 
 app.use('/upload', uploadRouter);
+app.use('/media-streaming', MediaStreamingRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
