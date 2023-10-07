341d3303b4974e2b7c599526d6429c7708948b10
rohit-simform <rohit.s@simformsolutions.com>
2023-10-07 20:34:06 +0530

    add IVS streaming player UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import UploadImage from "../src/components/UploadImage";
+import MediaStreaming from "./components/media-streaming";
 function App() {
   return (
     <>
       <div>
         <UploadImage />
+        <hr/>
+        <MediaStreaming/>
       </div>
     </>
   );
---
+++ frontend/src/components/media-streaming/index.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable no-unused-vars */
+
+import React, { useState } from "react";
+import Player from "./IVSPlayerComponent";
+import { Button, Input } from "antd";
+import axios from "axios";
+
+export default function MediaStreaming() {
+  const [channelArn, setChannelArn] = useState("");
+  const [channelName, setChannelName] = useState("");
+
+  const handleStartClick = async () => {
+    // Make an API request to start the IVS channel and get the channelArn
+    const { data } = await axios.get(
+      "http://localhost:5000/media-streaming/start"
+    );
+    // Set the channelArn in state here
+    setChannelArn(data.channelArn);
+  };
+
+  const handleStopClick = async () => {
+    // Make an API request to stop the IVS channel
+    const { data } = await axios.get(
+      "http://localhost:5000/media-streaming/stop"
+    );
+
+    // Clear the channelArn in state here
+    setChannelArn("");
+  };
+
+  return (
+    <div>
+      <h1>Amazon IVS Streaming Player</h1>
+      {channelArn ? (
+        <>
+          <Button onClick={handleStopClick}>Stop Streaming</Button>{" "}
+          <Player streamKey={channelArn} />
+        </>
+      ) : (
+        <>
+          <div style={{display:"flex", justifyContent:"space-evenly", alignItems:"center"}}>
+            <span>Enter Channel : </span>
+            <Input
+              value={channelName}
+              onChange={(e) => setChannelName(e.target.value)}
+            />
+          </div>
+          <Button onClick={handleStartClick} disabled={!channelName}>Start Streaming</Button>
+        </>
+      )}
+    </div>
+  );
+}
